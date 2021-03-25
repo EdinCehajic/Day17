70ad766afe124b55982ac6460200dbb0a78e46f8
EdinCehajic <ed.cehajic@gmail.com>
2021-03-25 09:07:30 +0100

    Adding

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function removeFirstTwo(list) {
+    "use strict";
+
+    // Only
+    const[a, b, ...array2] = list;
+
+    // Only
+    return array2;
+}
+
+console.log(removeFirstTwo(array1));
+module.exports = removeFirstTwo;
